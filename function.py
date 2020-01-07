f69e5ff4a31238d7e615a86f6e5adc1b588cdd18
hirano-wataru <42793984+hirano-wataru@users.noreply.github.com>
2020-01-07 22:22:13 +0900

    add function.py

---
+++ function.py
@@ -0,0 +1,17 @@
+# python /Users/hirano.wataru/be_professional_python/function.py
+# coding: utf_8
+
+# 関数
+def f(x):
+    # 関数定義のときにreturnを書かないとnoneが表示される。
+
+    return x*2
+
+def d(x=2):
+    return 2
+
+result =  f(10)
+print(result)
+
+d_result = d()
+print(d_result)
